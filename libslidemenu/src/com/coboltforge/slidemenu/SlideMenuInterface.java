df7c48e94546e36f437f1e2e543930f80c9ef19e
Christian Beier <dontmind@freeshell.org>
2012-07-18 00:02:28 +0200

    Add license boilerplates.

--- libslidemenu/src/com/coboltforge/slidemenu/SlideMenuInterface.java
+++ libslidemenu/src/com/coboltforge/slidemenu/SlideMenuInterface.java
@@ -1,3 +1,24 @@
+/*
+ * A sliding menu for Android, very much like the Google+ and Facebook apps have.
+ * 
+ * Copyright (C) 2012 CoboltForge
+ * 
+ * Based upon the great work done by stackoverflow user Scirocco (http://stackoverflow.com/a/11367825/361413), thanks a lot!
+ * The XML parsing code comes from https://github.com/darvds/RibbonMenu, thanks!
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.coboltforge.slidemenu;
 
 public interface SlideMenuInterface {
